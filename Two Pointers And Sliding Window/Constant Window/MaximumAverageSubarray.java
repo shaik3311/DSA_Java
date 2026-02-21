64753d67fbe3f6da1f631ed28982790f43c26c7a
shaik3311 <162719241+shaik3311@users.noreply.github.com>
2026-02-21 12:41:07 +0530

    initial commit

---
+++ Two Pointers And Sliding Window/Constant Window/MaximumAverageSubarray.java
@@ -0,0 +1,34 @@
+import java.util.*;
+
+public class MaximumAverageSubarray{
+    public static void main(String args[]){
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();  //size 
+        int k = sc.nextInt();  //window size
+        int[] arr = new int[n]; //array creation 
+        for(int i=0;i<n;i++){
+            arr[i] = sc.nextInt();  //array input
+        }
+
+        int left = 0;
+        int right = k-1;
+        double max_avg = Double.NEGATIVE_INFINITY;
+
+        // Brute Force method with TC->O(n*k), SC->O(1) 
+        
+        // while(right<n){
+        //     double sum = 0;
+        //     for(int i=left;i<=right;i++){
+        //         sum += arr[i];
+        //     }
+        //     double avg = sum/(right-left+1);
+        //     max_avg = Math.max(max_avg,avg);
+
+        //     left++;
+        //     right++;
+        // }
+
+        System.out.println(max_avg);
+
+    }
+}
