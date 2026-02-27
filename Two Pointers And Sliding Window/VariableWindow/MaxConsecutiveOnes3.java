50d3915bba13935e6b66232cda8bfad767fbb4fc
shaik3311 <162719241+shaik3311@users.noreply.github.com>
2026-02-27 20:55:32 +0530

    initial commit

--- Two Pointers And Sliding Window/VariableWindow/MaxConsecutiveOnes3.java
+++ Two Pointers And Sliding Window/VariableWindow/MaxConsecutiveOnes3.java
@@ -14,16 +14,30 @@ public class MaxConsecutiveOnes3{
 
         // Brute Force With TC->O(n*n) SC->O(1) 
         int max = 0;
-        for(int i=0;i<n;i++){
-            int zeros = 0;
-            for(int j=i;j<n;j++){
-                if(arr[j]==0) zeros++;
-                if(zeros<=k){
-                    max = Math.max(max,j-i+1);
-                }else{
-                    break;
-                }
+        // for(int i=0;i<n;i++){
+        //     int zeros = 0;
+        //     for(int j=i;j<n;j++){
+        //         if(arr[j]==0) zeros++;
+        //         if(zeros<=k){
+        //             max = Math.max(max,j-i+1);
+        //         }else{
+        //             break;
+        //         }
+        //     }
+        // }
+
+        Better Approach With TC->O(2n) SC->O(1);
+        int zeros = 0;
+        int left = 0;
+        int right = 0;
+        while(right<n){
+            if(arr[right]==0) zeros++;
+            while(zeros>k){
+                if(arr[left]==0) zeros--;
+                left++;
             }
+            max = Math.max(max,right-left+1);
+            right++;
         }
 
         System.out.println(max);
