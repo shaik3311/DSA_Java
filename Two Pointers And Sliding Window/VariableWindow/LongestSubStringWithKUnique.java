825928d41f89406e2a030501ece71a589fb591db
shaik3311 <162719241+shaik3311@users.noreply.github.com>
2026-02-28 22:58:57 +0530

    initial commit

--- Two Pointers And Sliding Window/VariableWindow/LongestSubStringWithKUnique.java
+++ Two Pointers And Sliding Window/VariableWindow/LongestSubStringWithKUnique.java
@@ -20,6 +20,28 @@ public class LongestSubStringWithKUnique{
         //     }
         // }
 
+
+        // Better Approach With TC->O(n+n) SC->O(3) 
+        int max = 0;
+        int left = 0;
+        int right = 0;
+        HashMap<Character,Integer> map = new HashMap<>();
+        while(right<s.length()){
+            map.put(s.charAt(right),map.getOrDefault(s.charAt(right),0)+1);
+            if(map.size()>k){
+                while(map.size()>k){
+                    map.put(s.charAt(left),map.getOrDefault(s.charAt(left),0)-1);
+                    if(map.get(s.charAt(left))<=0){
+                        map.remove(s.charAt(left));
+                    }
+                    left++;
+                }
+            }else{
+                max = Math.max(max,right-left+1);
+            }
+            right++;
+        }
+
         System.out.println(max);
     }
 }
