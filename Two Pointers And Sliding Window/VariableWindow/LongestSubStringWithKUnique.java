4db02161c7e6e7e7f2fb02a05c6c9f7b6a5520d3
shaik3311 <162719241+shaik3311@users.noreply.github.com>
2026-02-28 22:40:41 +0530

    initial commit

---
+++ Two Pointers And Sliding Window/VariableWindow/LongestSubStringWithKUnique.java
@@ -0,0 +1,25 @@
+import java.util.*;
+
+public class LongestSubStringWithKUnique{
+    public static void main(String args[]){
+        Scanner sc = new Scanner(System.in);
+        String s = sc.nextLine();
+        int k = sc.nextInt();
+
+
+        int max = 0;
+        for(int i=0;i<s.length();i++){
+            HashSet<Character> set = new HashSet<>();
+            for(int j=i;j<s.length();j++){
+                set.add(s.charAt(j));
+                if(set.size()<=k){
+                    max = Math.max(max,j-i+1);
+                }else{
+                    break;
+                }
+            }
+        }
+
+        System.out.println(max);
+    }
+}
