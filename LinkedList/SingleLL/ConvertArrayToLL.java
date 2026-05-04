2a439f5666fc3a75ebc333247b79bed85d2ec2b7
shaik3311 <162719241+shaik3311@users.noreply.github.com>
2026-05-04 16:24:11 +0530

    Linked List

--- LinkedList/SingleLL/ConvertArrayToLL.java
+++ LinkedList/SingleLL/ConvertArrayToLL.java
@@ -14,6 +14,19 @@ class Node{
 }
 
 public class ConvertArrayToLL {
+    public static boolean search(Node head,int elem){
+        Node temp = head;
+        while(temp!=null){
+            if(temp.data==elem) return true;
+            temp = temp.next;
+        }
+        return false;
+    }
+    public static Node deleteHead(Node head){
+        if(head==null) return head;
+        head = head.next;
+        return head;
+    }
     public static void main(String[] args) {
         int[] arr = {8,1,2,5,4,9};
         Node head = new Node(arr[0]);
@@ -24,28 +37,20 @@ public class ConvertArrayToLL {
             mover = temp;
         }
         // Traversing the linked list
-        // Node temp = head;
-        // while(temp!=null){
-        //     System.out.print(temp.data+" -> ");
-        //     if(temp.next== null) System.out.print("null");
-        //     temp = temp.next;
-        // }
-
-        // Length of the linked list
-        int len = 0;
-        Node temp2 = head;
-        while(temp2!=null){
-            len++;
-            temp2 = temp2.next;
-        }
-        System.out.println(search(head, 22));
-    }
-    public static boolean search(Node head,int elem){
         Node temp = head;
         while(temp!=null){
-            if(temp.data==elem) return true;
+            System.out.print(temp.data+" -> ");
+            if(temp.next== null) System.out.print("null");
             temp = temp.next;
         }
-        return false;
+
+        // Length of the linked list
+        // int len = 0;
+        // Node temp2 = head;
+        // while(temp2!=null){
+        //     len++;
+        //     temp2 = temp2.next;
+        // }
     }
+    
 }
